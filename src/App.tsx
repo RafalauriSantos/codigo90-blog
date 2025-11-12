775289d326cd49faa0712c67acb02146d7250d00
Rafa_Dev <rafa69lauri@gmail.com>
2025-11-12 20:22:04 -0300

    Fix: Usar injectSpeedInsights para Vite

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import ComoAprenderProgramacao from "./pages/blog/ComoAprenderProgramacao";
@@ -16,7 +15,6 @@ const App = () => (
 		<TooltipProvider>
 			<Toaster />
 			<Sonner />
-			<SpeedInsights />
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Index />} />
